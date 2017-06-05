a7e19d738a4da799934f4e9581e2f0f532ece028
jihaifeng <1104128141@qq.com>
2017-06-05 18:21:08 +0800

    up

---
+++ src/jihf/kotlin/Kotlin02.kt
@@ -0,0 +1,18 @@
+package jihf.kotlin
+
+/**
+ * Func：
+ * Desc:
+ * Author：JHF
+ * Data：2017-06-05 17:38
+ * Mail：jihaifeng@raiyi.com
+ */
+fun main(args: Array<String>) {
+    var quantity = 5
+    val price: Double = 20.3
+    val name: String = "jihaifeng"
+    println("单价：$quantity")
+    println("价格：$price")
+    println("姓名：$name")
+
+}
