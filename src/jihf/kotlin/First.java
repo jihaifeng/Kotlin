ea1bf151d6748dc1dae35c2b7015c739e49fef09
jihaifeng <1104128141@qq.com>
2017-06-05 15:25:36 +0800

    up

---
+++ src/jihf/kotlin/First.java
@@ -0,0 +1,7 @@
+package jihf.kotlin;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.print("hello java");
+    }
+}
---
+++ src/jihf/kotlin/Kotlin01.kt
@@ -0,0 +1,5 @@
+package jihf.kotlin
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin")
+}
